a0ad77f950e5aac692ac504f146ebf4d3d587878
Edyta2801 <edyta.szarowska@interia.pl>
2019-10-21 14:50:45 +0200

    add fetch to sendOrder

--- src/js/script.js
+++ src/js/script.js
@@ -396,6 +396,8 @@
 
       thisCart.renderTotalsKeys = ['totalNumber', 'totalPrice', 'subtotalPrice', 'deliveryFee'];
 
+      thisCart.dom.form = thisCart.dom.wrapper.querySelector(select.cart.form);
+
       for (let key of thisCart.renderTotalsKeys) {
         thisCart.dom[key] = thisCart.dom.wrapper.querySelectorAll(select.cart[key]);
       }
@@ -409,6 +411,10 @@
       thisCart.dom.productList.addEventListener('updated', function () { thisCart.update(); });
 
       thisCart.dom.productList.addEventListener('remove', function () { thisCart.remove(event.detail.cartProduct); });
+      thisCart.dom.form.addEventListener('submit', function (event) {
+        event.preventDefault();
+        thisCart.sendOrder();
+      });
 
     }
     add(menuProduct) {
@@ -462,6 +468,30 @@
       thisCart.products.splice(index, 1);
       cartProduct.dom.wrapper.remove();
       thisCart.update();
+    }
+    sendOrder() {
+      const thisCart = this;
+
+      const url = settings.db.url + '/' + settings.db.order;
+
+      const payload = {
+        address: 'test',
+        totalPrice: thisCart.totalPrice,
+      };
+
+      const options = {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify(payload),
+      };
+      fetch(url, options)
+        .then(function (response) {
+          return response.json();
+        }).then(function (parsedResponse) {
+          console.log('parsedResponse', parsedResponse);
+        });
     }
   }
 
@@ -565,8 +595,8 @@
         .then(function (parsedResponse) {
           console.log('parsedResponse', parsedResponse);
           // save parsedResponse as thisApp.data.products
-          thisApp.data.products=parsedResponse;
-          console.log(parsedResponse);
+          thisApp.data.products = parsedResponse;
+          // console.log(parsedResponse);
           // execute initMenu method
           thisApp.initMenu(parsedResponse);
         });
