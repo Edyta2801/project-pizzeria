15e23e70815780df6fb05f2ca9a6cd45b1361834
Edyta2801 <edyta.szarowska@interia.pl>
2019-10-23 10:22:52 +0200

    modify files

--- dist/js/app.js
+++ dist/js/app.js
@@ -1,72 +1,61 @@
-/* global Handlebars, utils, dataSource */ // eslint-disable-line no-unused-vars
 
-{
-  'use strict';
+const app = {
+  initMenu: function () {
+    const thisApp = this;
+    // console.log('thisApp.data:', thisApp.data);
 
+    for (let productData in thisApp.data.products) {
+      new Product(thisApp.data.products[productData].id, thisApp.data.products[productData]);
+    }
+    // const testProduct = new Product();
+    // console.log('testProduct:', testProduct);
+  },
+  initData: function () {
+    const thisApp = this;
+    thisApp.data = {};
+    const url = settings.db.url + '/' + settings.db.product;
+    console.log(url);
 
+    fetch(url)
+      .then(function (rawResponse) {
+        return rawResponse.json();
+      })
+      .then(function (parsedResponse) {
+        console.log('parsedResponse', parsedResponse);
+        // save parsedResponse as thisApp.data.products
+        thisApp.data.products = parsedResponse;
+        // console.log(parsedResponse);
+        // execute initMenu method
+        thisApp.initMenu(parsedResponse);
+      });
+    console.log('thisApp.data', JSON.stringify(thisApp.data));
 
+  },
+  initCart: function () {
+    const thisApp = this;
 
+    const cartElem = document.querySelector(select.containerOf.cart);
+    // console.log(cartElem);
 
+    // instancja klasy Cart zapisana  w thisApp.cart, oznacza że można wywołać ją poza obiektem app za pomocą app.cart
+    thisApp.cart = new Cart(cartElem);
 
+  },
 
-
-  const app = {
-    initMenu: function () {
-      const thisApp = this;
-      // console.log('thisApp.data:', thisApp.data);
-
-      for (let productData in thisApp.data.products) {
-        new Product(thisApp.data.products[productData].id, thisApp.data.products[productData]);
-      }
-      // const testProduct = new Product();
-      // console.log('testProduct:', testProduct);
-    },
-    initData: function () {
-      const thisApp = this;
-      thisApp.data = {};
-      const url = settings.db.url + '/' + settings.db.product;
-      console.log(url);
-
-      fetch(url)
-        .then(function (rawResponse) {
-          return rawResponse.json();
-        })
-        .then(function (parsedResponse) {
-          console.log('parsedResponse', parsedResponse);
-          // save parsedResponse as thisApp.data.products
-          thisApp.data.products = parsedResponse;
-          // console.log(parsedResponse);
-          // execute initMenu method
-          thisApp.initMenu(parsedResponse);
-        });
-      console.log('thisApp.data', JSON.stringify(thisApp.data));
-
-    },
-    initCart: function () {
-      const thisApp = this;
-
-      const cartElem = document.querySelector(select.containerOf.cart);
-      // console.log(cartElem);
-
-      // instancja klasy Cart zapisana  w thisApp.cart, oznacza że można wywołać ją poza obiektem app za pomocą app.cart
-      thisApp.cart = new Cart(cartElem);
-
-    },
-
-    init: function () {
-      const thisApp = this;
-      console.log('*** App starting ***');
-      console.log('thisApp:', thisApp);
-      console.log('classNames:', classNames);
-      console.log('settings:', settings);
-      console.log('templates:', templates);
+  init: function () {
+    const thisApp = this;
+    console.log('*** App starting ***');
+    console.log('thisApp:', thisApp);
+    console.log('classNames:', classNames);
+    console.log('settings:', settings);
+    console.log('templates:', templates);
 
 
-      thisApp.initData();
-      // thisApp.initMenu();
-      thisApp.initCart();
-    },
-  };
+    thisApp.initData();
+    // thisApp.initMenu();
+    thisApp.initCart();
+  },
+};
 
-  app.init();
-}
+app.init();
+
